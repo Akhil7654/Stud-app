8ab53c93b42bb3a59428bfb4352b195f8eaf584f
Akhil7654 <msdcr88@gmail.com>
2022-07-08 11:46:24 +0530

    done2

--- src/app/viewstudent/viewstudent.component.ts
+++ src/app/viewstudent/viewstudent.component.ts
@@ -12,4 +12,10 @@ export class ViewstudentComponent implements OnInit {
   ngOnInit(): void {
   }
           hii="love you babee"
+          data=[
+            {"name":"you","age":18,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfZDabpYnE3St-xoZZgVSGgRMIn3km-RjzE4YooiGV5R4kklAzNzb8GVZwLGCHzPW4puQ&usqp=CAU"},
+            {"name":"Srinidhi","age":26,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKLZqE5WBpfpFIMnQHeb1zWVgs1dfz-Ytf-A&usqp=CAU"},
+            {"name":"Priyanka","age":24,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrznOeW0WCEdK_kQMCcYRQEGQqgtMyXTunPQ&usqp=CAU"},
+            {"name":"Krithi","age":18,"photo":"https://static.toiimg.com/thumb/msid-89074835,width-900,height-1200,resizemode-6.cms"}
+          ]
 }
